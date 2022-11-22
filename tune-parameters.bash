f5e1a10bb44a69df81a017104ec5749f4f10f3bb
Alan Liu <Alan2001liu09@yahoo.com>
2022-11-22 19:54:15 +0000

    Tune parameters

--- tune-parameters.bash
+++ tune-parameters.bash
@@ -1,14 +1,16 @@
 #!/bin/bash
 lr=0.01
 
-for clip_len in 16 24 32
+for clip_len in 16
 do
     for ca in 0.1 0.3 0.5 0.7 0.9
     do
-        for cw in 0.1 1.0
+        for cw in 0.1
         do
             file_name="results/results-lr-$lr-clip-len-$clip_len-ca-$ca-cw-$cw.txt"
-            python train-radar.py --lr=$lr --clip-len=$clip_len --contrastive-alpha=$ca --contrastive-weight=$cw  --result-file=$file_name
+            if [ ! -f ${file_name} ]; then
+                python train-radar.py --lr=$lr --clip-len=$clip_len --contrastive-alpha=$ca --contrastive-weight=$cw  --result-file=$file_name
+            fi
         done
     done
 done
